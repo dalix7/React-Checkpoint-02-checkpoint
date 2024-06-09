c295efa141fa0c06720416587940427efc37b7a0
dalix <letaiefdali@gmail.com>
2024-06-09 18:17:26 +0100

    playerList

---
+++ src/Components/players.js
@@ -0,0 +1,42 @@
+const players = [
+  {
+    name: <h2> "Cristiano Ronaldo" </h2>,
+    team: "alNaser",
+    nationality: "Portoguese",
+    player_number: "7",
+    age: "38",
+    Img_URL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Ronaldo_in_2018.jpg/249px-Ronaldo_in_2018.jpg",
+  },
+
+  {
+    name: <h2> "Mbappé" </h2>,
+    team: "Real Madrid",
+    nationality: "france",
+    player_number: "9",
+    age: "24",
+    Img_URL:
+      "https://e0.365dm.com/24/02/2048x1152/skysports-kylian-mbappe-psg_6462523.jpg?20240220164300",
+  },
+
+  {
+    name: <h2> "rodrigo"</h2>,
+    team: "Real Madrid",
+    nationality: "brasil",
+    player_number: "21",
+    age: "21",
+    Img_URL:
+      "https://assets-fr.imgfoot.com/media/cache/1200x1200/rodrygo-2324.jpg",
+  },
+
+  {
+    name: <h2> "luka modrić" </h2>,
+    team: "Real Madrid",
+    nationality: "Coratia",
+    player_number: "10",
+    age: "33",
+    Img_URL:
+      "https://c.ndtvimg.com/2022-06/rb7l2pr8_luka-modric-afp_625x300_08_June_22.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605",
+  },
+];
+export default players;
